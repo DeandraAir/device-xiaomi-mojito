1948e319cd576a79798484b2c2b134eaafea7451
Deandra Aditya <142644567+DeandraAir@users.noreply.github.com>
2024-02-12 03:36:15 +0700

    mojito: Bringup for Matrixx

--- lineage_mojito.mk
+++ lineage_mojito.mk
@@ -11,11 +11,11 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from mojito device
 $(call inherit-product, device/xiaomi/mojito/device.mk)
 
-# Inherit some common BlissRoms stuff.
-$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
+# Inherit some common LineageOS stuff.
+$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 
 # Product Specifics
-PRODUCT_NAME := bliss_mojito
+PRODUCT_NAME := lineage_mojito
 PRODUCT_DEVICE := mojito
 PRODUCT_BRAND := Redmi
 PRODUCT_MODEL := Redmi Note 10
@@ -25,6 +25,12 @@ PRODUCT_MANUFACTURER := Xiaomi
 TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_FACE_UNLOCK_SUPPORTED := true
 TARGET_INCLUDE_PIXEL_CHARGER := true
-BLISS_BUILDTYPE := OFFICIAL
+
+# Matrixx
+MATRIXX_MAINTAINER := Deandra_Aditya
+MATRIXX_CHIPSET := SDM678
+MATRIXX_BATTERY := 5000mAh
+MATRIXX_DISPLAY := 1080x2400
+WITH_GMS := 
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
